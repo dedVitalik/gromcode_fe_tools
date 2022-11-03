dea9b8219e75c3775eb4d4d555e7b59da4008871
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-11-03 22:56:44 +0100

    lesson 6 task 1 now the end )

--- lesson_6/task_1/webpack.config.js
+++ lesson_6/task_1/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                         options: {
                             limit: 8192,
                             name: '[name].[ext]',
-                            outputPath: 'images/',
+                            outputPath: 'images',
                         }
                     }
                 ]
