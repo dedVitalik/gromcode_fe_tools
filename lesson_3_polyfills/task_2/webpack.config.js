1a9191dcc3554a3060ca73347fddb075de02c676
Vitaliy Vynnyk <vitaliy.vinnik@gmail.com>
2022-11-03 20:41:48 +0100

    lesson 6 task 3 attempt 2

--- lesson_3_polyfills/task_2/webpack.config.js
+++ lesson_3_polyfills/task_2/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                         options: {
                             limit: 8192,
                             name: '[name].[ext]',
-                            outputPath: 'images/',
+                            outputPath: 'images',
                         }
                     }
                 ]
